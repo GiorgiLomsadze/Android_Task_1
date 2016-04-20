6944f5231a17cb8eea84e66510734091631d2f04
Giorgi Lomsadze <giorgi.lomsadze03@geolab.edu.ge>
2016-04-20 22:38:17 +0400

    Add Rotation

--- Flashlight/app/src/main/java/assign1/lomsadze/giorgi/flashlight/MainActivity.java
+++ Flashlight/app/src/main/java/assign1/lomsadze/giorgi/flashlight/MainActivity.java
@@ -7,25 +7,42 @@ import android.view.View;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int color = Color.WHITE;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null){
+            color = savedInstanceState.getInt("Color");
+            LinearLayout layout = (LinearLayout) findViewById(R.id.mtavariLayaut);
+            layout.setBackgroundColor(color);
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("Color", color);
     }
 
     public void onClick(View v){
         LinearLayout layout = (LinearLayout) findViewById(R.id.mtavariLayaut);
         if (v.getId() == R.id.redBtn){
             layout.setBackgroundColor(Color.RED);
+            color = Color.RED;
         }else if (v.getId() == R.id.blueBtn){
           layout.setBackgroundColor(Color.BLUE);
+            color = Color.BLUE;
         }else if (v.getId() == R.id.greenBtn){
             layout.setBackgroundColor(Color.GREEN);
+            color = Color.GREEN;
         }else if (v.getId() == R.id.witeBtn){
             layout.setBackgroundColor(Color.WHITE);
+            color = Color.WHITE;
         }else if (v.getId()== R.id.yelloBtn){
             layout.setBackgroundColor(Color.YELLOW);
+            color = Color.YELLOW;
         }
     }
 }
